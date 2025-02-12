ac30da7a542998198a7d4410f3fce9a506eb58ed
ThiThs <thiago.souza6@hotmail.com>
2025-02-11 22:20:17 -0300

    Primeiro Commit

---
+++ polls/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+    def __str__(self):
+        return self.question_text
+
+def has_votes(self):
+        return self.choice_set.filter(votes__gt=0).exists()
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
