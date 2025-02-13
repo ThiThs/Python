6bcb36b80e752719316b374e41837ad85edc19a6
ThiThs <thiago.souza6@hotmail.com>
2025-02-13 19:42:07 -0300

    git

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+from .models import Question, Choice
 
-from .models import Question
+class ChoiceInline(admin.TabularInline):  
+    model = Choice  
+    extra = 2  
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):  
+    list_display = ('question_text', 'total_votes','has_votes')  
+
+    def total_votes(self, obj):
+        return obj.total_votes()
+    
+    total_votes.short_description = _("Total de Votos")  
+
+    def has_votes(self, obj):
+        return obj.has_votes()
+    
+    has_votes.short_description = _("Contém votos?")
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
--- polls/models.py
+++ polls/models.py
@@ -1,18 +1,27 @@
+import datetime
 from django.db import models
-
+from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
-    def __str__(self):
+    
+    def __str__(self): 
         return self.question_text
+    
+    def total_votes(self):
+        return self.choice_set.aggregate(models.Sum('votes'))['votes__sum'] or 0
 
-def has_votes(self):
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    
+    def has_votes(self):
         return self.choice_set.filter(votes__gt=0).exists()
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-    def __str__(self):
+    def __str__(self): 
         return self.choice_text
